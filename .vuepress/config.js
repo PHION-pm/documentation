8603e53a56c96719793413eb0f98260c57ee80b8
Aditya Gupta <aditsuyag@gmail.com>
2022-02-06 10:15:30 +0530

    (init): Setup VuePress

---
+++ .vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'PHION',
+    description: 'Documentation for PHION Rice Package Manager',
+    themeConfig: {
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Download', link: '/'},
+            {text: 'Github', link: '/'},
+        ]
+    }
+  }
